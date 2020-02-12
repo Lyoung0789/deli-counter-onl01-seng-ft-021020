c542d17ce9bb286ae9fb9ec0aaac278488dae0d8
Lester Young <lyoung070789@gmail.com>
2020-02-12 00:09:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,7 @@ def line(array)
   if array.length==0
     puts "The line is currently empty."
   else array.length>0
+    while 0 < array.length
     array.each_with_index{|name, position| puts "The line is currently: #{position+1}. #{name}"}
   end
 end
