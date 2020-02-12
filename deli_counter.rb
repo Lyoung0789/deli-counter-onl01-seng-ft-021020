52c38cb4b7a8424526b2253bdcffd476b85c224a
Lester Young <lyoung070789@gmail.com>
2020-02-12 00:35:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,5 @@
+#if array is empty give prompt 
+#else created a new array to hold the names of the people in line and their positions. Then turned the new_line into a string by using .join and outout prompt.
 def line(array)
   if array.length==0
     puts "The line is currently empty."
@@ -8,12 +10,18 @@ def line(array)
   end
 end
 
+
+#add the name to back of line using push or <<
+#find the length of the line and output prompt.
 def take_a_number(array, name)
   array << name
   position = array.length
   puts "Welcome, #{name}. You are number #{position} in line."
 end
 
+
+#if there is noone in line outpit the given prompt
+#if there are people in line output index 0 of array and use array.shift to remove them from array.
 def now_serving(array)
   if array.length == 0
     puts "There is nobody waiting to be served!"
