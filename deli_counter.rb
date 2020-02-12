fddc8357f290ce85696302e3c0b9f79b2c82979f
Lester Young <lyoung070789@gmail.com>
2020-02-12 00:30:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,3 +14,11 @@ def take_a_number(array, name)
   puts "Welcome, #{name}. You are number #{position} in line."
 end
 
+def now_serving(array)
+  if array.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array[0]}"
+    array.shift
+  end
+end
