ec83963254eaba6b80b6af73cd392151fcc403c0
Lester Young <lyoung070789@gmail.com>
2020-02-12 00:14:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++
@@ -1,7 +0,0 @@
-def line(array)
-  if array.length==0
-    puts "The line is currently empty."
-  else array.length>0
-    array.each_with_index{|name, position| puts "The line is currently: #{position+1}. #{name}"}
-  end
-end
